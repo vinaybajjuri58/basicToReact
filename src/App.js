889911fd55624857e1ab8b53b18482878eda1d6c
vinaybajjuri58 <vinaybajjuri58@github.com>
2020-06-15 09:53:39 +0530

    Version 2 with jumbotron section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navabar from './Components/Navbar'
 import Footer from './Components/Footer'
+import Jumbotron from './Components/Jumbotron'
 
 const App =()=>{
     return(
         <div>
             <Navabar />
+            <Jumbotron />
             <h1>Vinay </h1>
             <Footer />
         </div>
