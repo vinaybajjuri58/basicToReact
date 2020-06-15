f21292bd325ac484ab462f6cbf0e630df3ac6733
vinaybajjuri58 <vinaybajjuri58@github.com>
2020-06-15 12:10:44 +0530

    All components added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Navabar from './Components/Navbar'
 import Footer from './Components/Footer'
 import Jumbotron from './Components/Jumbotron'
+import Feature from './Components/Feature'
+import Contact from './Components/Contact'
+// import Addition from './Addition'
 
 const App =()=>{
     return(
         <div>
             <Navabar />
             <Jumbotron />
-            <h1>Vinay </h1>
+            {/* <h1>Vinay </h1> */}
+            {/* <Addition /> */}
+            <Feature />
+            <Contact />
             <Footer />
         </div>
     )
